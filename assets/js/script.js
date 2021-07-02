43a5f47764b7a9f534f607245cc0c2b426ee551d
richie-escobedo <richie_escobedo@aol.com>
2021-07-01 20:49:02 -0700

    created windows confirm and prompt for password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
 // Assignment code here
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvqxyz"; 
-var numbers = "0123456789";
-var special = "!#$%&'()*+,-./:;<=>?@_`{|}~";
-var userChoice = "";
+    var userChoice = "";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvqxyz"; 
+    var numbers = "0123456789";
+    var special = "!#$%&'()*+,-./:;<=>?@_`{|}~";
+
+function generatePassword () {
+    var pwLength = window.prompt("Enter between 8 - 128 characters for your password?");
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    // confirm password criteria 
+    var upperConfirm = window.confirm("Include uppercase letters?");
+    var lowerConfirm = window.confirm("Include lowercase letters?");
+    var numbersConfirm = window.confirm("Include numbers?");
+    var specialConfirm = window.confirm("Include special characters?");
+    
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    // Get references to the #generate element
+    var generateBtn = document.querySelector("#generate");
+
+    // Write password to the #password input
+    function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+
+      passwordText.value = password;
+    }
+        // Add event listener to generate button
+        generateBtn.addEventListener("click", writePassword);
+  
