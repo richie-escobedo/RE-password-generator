b7028c2f8dab7f75586a3396b39ac26fc4e3278a
richie-escobedo <richie_escobedo@aol.com>
2021-07-01 21:20:47 -0700

    added all if statements for criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,41 @@
     var lower = "abcdefghijklmnopqrstuvqxyz"; 
     var numbers = "0123456789";
     var special = "!#$%&'()*+,-./:;<=>?@_`{|}~";
+    var userChoice = "";
 
 function generatePassword () {
+
     var pwLength = window.prompt("Enter between 8 - 128 characters for your password?");
 
-
+  
 
     // confirm password criteria 
     var upperConfirm = window.confirm("Include uppercase letters?");
-    var lowerConfirm = window.confirm("Include lowercase letters?");
-    var numbersConfirm = window.confirm("Include numbers?");
-    var specialConfirm = window.confirm("Include special characters?");
-    
+      if (upper) {
+        userChoice += upper;
+    };
 
+    var lowerConfirm = window.confirm("Include lowercase letters?");
+      if (lower) {
+        userChoice += lower;
+    };
+
+    var numbersConfirm = window.confirm("Include numbers?");
+      if (numbers) {
+        userChoice += numbers;
+    };
+
+    var specialConfirm = window.confirm("Include special characters?");
+      if (special) {
+        userChoice += special;
+    };
+
+    var pw = "";
+      for (let i = 0; i < pwlength; i++) {
+        pw += userChoice[Math.floor(Math.random() * userChoice.length)]
+      }
+
+      return pw;
 }
 
     // Get references to the #generate element
